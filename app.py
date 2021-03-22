28a22a4d51a4d0ce2d8cb8ebc58dcae3e1bfc5a3
mikkelam <mikkelarm@gmail.com>
2021-03-22 15:38:09 +0100

    refactor into async generator

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
-    success = asyncio.run(main(event))
-
+    success = asyncio.run(main(event), debug=True)
     response = {
         "statusCode": 200,
         "success": success
@@ -23,43 +22,48 @@ def handler(event, context):
 async def main(event):
     images = event['image_urls']
     upload_url = event['upload_url']
-    queue = asyncio.Queue(maxsize=100)
-    produce = asyncio.create_task(img_downloader(images, queue))
-    video_binary = await make_video(queue)
 
-    logger.info(f"Uploading img")
+    video_binary = await make_video(images)
+
+    logger.info(f"Uploading video")
     r = httpx.post(upload_url, content=video_binary,
                    headers={"content-type": "video/mp4"}
                    )
     return r.status_code == 200
 
 
-async def img_downloader(img_urls, queue):
-    for img_url in img_urls:
-        logger.info(f"Downloading {img_url}")
-        r = httpx.get(img_url)
+async def img_downloader(img_urls):
+    client = httpx.AsyncClient(http2=True)
+    semaphore = asyncio.Semaphore(10)
 
-        image = Image.open(io.BytesIO(r.content))
-        await queue.put(image)
-    await queue.put(None)
-    return
+    async def get_image(url):
+        async with semaphore:
+            resp = await client.get(url)
+            return Image.open(resp)
+
+    tasks = [asyncio.create_task(get_image(img_url))
+             for img_url in img_urls]
+
+    for t in tasks:
+        yield t
+
+    await client.aclose()
 
 
-async def make_video(queue):
-    with io.BytesIO() as buffer:
-        output = av.open(buffer, 'w', format='mp4', options={'crf': '18'})
-        stream = output.add_stream('h264', 24)
-        while True:
-            logger.info(f"Waiting for img")
-            item = await queue.get()
-            if item is None:
-                logger.info(f"Done!")
-                break
-
-            frame = av.VideoFrame.from_image(item)
-            packet = stream.encode(frame)
-            output.mux(packet)
-        packet = stream.encode(None)
+async def make_video(images):
+    buffer = io.BytesIO()
+    output = av.open(buffer, 'w', format='mp4', options={'crf': '18'})
+    stream = output.add_stream('h264', 24)
+    finished = 0
+    async for img in img_downloader(images):
+        img = await img
+        print(img)
+        frame = av.VideoFrame.from_image(img)
+        packet = stream.encode(frame)
         output.mux(packet)
+        finished += 1
+        logger.info(f'Finished frame {finished}')
+    packet = stream.encode(None)
+    output.mux(packet)
 
-        return buffer.read()
+    return buffer.read()
